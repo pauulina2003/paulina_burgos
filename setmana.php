253ba39855e979775bebebdc6eff809f861d60f1
Paulina <paulina>
2022-10-08 13:13:10 +0200

    M09

--- setmana.php
+++ setmana.php
@@ -1,3 +1,23 @@
 <?php
 $dia_angles = date('D');
-echo "El dia de la setmana és: $dia_angles";
+if ($dia_angles == "Sat"){
+    echo "El dia de la setmana és: Dissabte";
+}
+if ($dia_angles == "Sun"){
+    echo "El dia de la setmana és: Diumenge";
+}
+if ($dia_angles == "Mon"){
+    echo "El dia de la setmana és: Dilluns";
+}
+if ($dia_angles == "Tue"){
+    echo "El dia de la setmana és: Dimarts";
+}
+if ($dia_angles == "Wed"){
+    echo "El dia de la setmana és: Dimecres";
+}
+if ($dia_angles == "Thu"){
+    echo "El dia de la setmana és: Dijous";
+}
+if ($dia_angles == "Fri"){
+    echo "El dia de la setmana és: Divendres";
+}
