ca18e0a7bc9889ae14f0efe4cd9b05ceb3e1de6c
Paulina <paulina>
2022-10-08 12:53:05 +0200

    M09

--- multi.php
+++ multi.php
@@ -2,7 +2,7 @@
 
 $entrada = readline('Introdueix des de l\'entrada standard: ');
 echo "Taula de multiplicar del $entrada\n";
-for ($i = 0; $i < 12; $i++) {
+for ($i = 0; $i < 11; $i++) {
     $result = (int)$entrada * (int)$i;
     echo "$entrada * $i = $result\n";
 }
