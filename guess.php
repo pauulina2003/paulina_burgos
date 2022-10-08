a592f0f54174a30312bb96453526dc3f675e5ae3
Paulina <paulina>
2022-10-08 12:58:25 +0200

    M09

--- guess.php
+++ guess.php
@@ -3,12 +3,13 @@
 $num = rand(0,10);
 $intentos = 5;
 $entrada = -1;
-while ($entrada != $num || $intentos > 0){
+while ($entrada != $num || $intentos < 0){
     $entrada = readline("Adivina que un numero entre el 0 y el 10\n");
     if ($entrada != $num){
         $intentos -= 1;
         if ($intentos == 0){
             echo "Te has quedado sin intentos, el numero correcto era $num\n";
+            break;
         }else {
             echo "Ese no es, te quedan $intentos\n";
         }
