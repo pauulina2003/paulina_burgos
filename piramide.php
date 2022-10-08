07116ae9f529e5a7449d6b6cab59ffac62c3deec
Paulina <paulina>
2022-10-08 13:46:24 +0200

    M09

---
+++ piramide.php
@@ -0,0 +1,12 @@
+<?php
+echo "<p>&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+echo "<p>*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;</p>";
+?>
